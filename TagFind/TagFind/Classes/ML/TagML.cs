e210cc1d43fbec0cc5df5de7675abc66a6efaacd
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2026-01-06 21:27:33 +0800

    Update code

---
+++ TagFind/TagFind/Classes/ML/TagML.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagFind.Classes.ML
+{
+    public class TagML
+    {
+        private MLContext MLContext = new();
+        private ITransformer Model;
+
+        public class DocumentData
+        {
+            public string Content { get; set; } = string.Empty;
+            public List<long> TagID { get; set; } = [];
+        }
+
+    }
+}
