f8914e6c5e0e5893990501ffa20619859eea1ab5
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2026-03-31 21:27:21 +0800

    Add content to detail page

---
+++ TagFind/TagFind/Classes/Extensions/StorageSizeExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagFind.Classes.Extensions
+{
+    public static class StorageSizeExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="binaryUnits">true for 1024 per unit, false for 1000 per unit.</param>
+        /// <param name="storageBytes">Size using bytes unit.</param>
+        /// <returns></returns>
+        public static string ToSuitableStorageSize(this long storageBytes, bool binaryUnits)
+        {
+            if (binaryUnits)
+            {
+                if (storageBytes < Math.Pow(2, 10))
+                    return storageBytes.ToString() + " " + "bytes";
+                else if (storageBytes < Math.Pow(2, 20))
+                    return ((double)storageBytes / Math.Pow(2, 10)).ToString() + " " + "KiB";
+                else if (storageBytes < Math.Pow(2, 30))
+                    return ((double)storageBytes / Math.Pow(2, 20)).ToString() + " " + "MiB";
+                else if (storageBytes < Math.Pow(2, 40))
+                    return ((double)storageBytes / Math.Pow(2, 30)).ToString() + " " + "GiB";
+                else if (storageBytes < Math.Pow(2, 50))
+                    return ((double)storageBytes / Math.Pow(2, 40)).ToString() + " " + "TiB";
+                else if (storageBytes < Math.Pow(2, 60))
+                    return ((double)storageBytes / Math.Pow(2, 50)).ToString() + " " + "PiB";
+                else if (storageBytes < Math.Pow(2, 70))
+                    return ((double)storageBytes / Math.Pow(2, 60)).ToString() + " " + "EiB";
+                else
+                    return (storageBytes / Math.Pow(2, 70)).ToString() + " " + "ZiB";
+            }
+            else
+            {
+                if (storageBytes < Math.Pow(1000, 1))
+                    return storageBytes.ToString() + " " + "bytes";
+                else if (storageBytes < Math.Pow(1000, 2))
+                    return ((double)storageBytes / Math.Pow(1000, 1)).ToString() + " " + "KB";
+                else if (storageBytes < Math.Pow(1000, 3))
+                    return ((double)storageBytes / Math.Pow(1000, 2)).ToString() + " " + "MB";
+                else if (storageBytes < Math.Pow(1000, 4))
+                    return ((double)storageBytes / Math.Pow(1000, 3)).ToString() + " " + "GB";
+                else if (storageBytes < Math.Pow(1000, 5))
+                    return ((double)storageBytes / Math.Pow(1000, 4)).ToString() + " " + "TB";
+                else if (storageBytes < Math.Pow(1000, 6))
+                    return ((double)storageBytes / Math.Pow(1000, 5)).ToString() + " " + "PB";
+                else if (storageBytes < Math.Pow(1000, 7))
+                    return ((double)storageBytes / Math.Pow(1000, 6)).ToString() + " " + "EB";
+                else
+                    return ((double)storageBytes / Math.Pow(1000, 7)).ToString() + " " + "ZB";
+            }
+        }
+    }
+}
--- TagFind/TagFind/Pages/DBDetailedInformationPage.xaml.cs
+++ TagFind/TagFind/Pages/DBDetailedInformationPage.xaml.cs
@@ -10,6 +10,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using TagFind.Classes;
+using TagFind.Classes.DB;
+using TagFind.Classes.Extensions;
+using TagFind.Interfaces;
+using TagFind.Interfaces.IPageNavigationParameter;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -21,8 +26,34 @@ namespace TagFind.Pages
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class DBDetailedInformationPage : Page
+    public sealed partial class DBDetailedInformationPage : Page, IDBContentAccessiblePage
     {
+        public DBContentManager ContentManager { get; set; } = new();
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (e.Parameter is DBContentManager dBContentManager)
+            {
+                ContentManager = dBContentManager;
+            }
+            else if (e.Parameter is IDBContentManagerParameter dBContentManagerParameter)
+            {
+                if (dBContentManagerParameter.DBContentManager != null)
+                {
+                    ContentManager = dBContentManagerParameter.DBContentManager;
+                }
+            }
+
+            // Load info
+            DBNameTextBlock.Text = Path.GetFileNameWithoutExtension(ContentManager.DBPath);
+            long fileLength = new FileInfo(ContentManager.DBPath).Length;
+            StorageSizeValueTextBlock.Text = fileLength < 1024
+                ? fileLength + " " + "bytes"
+                : fileLength.ToSuitableStorageSize(SettingsValues.UseBinaryStorageUnits) + " (" + fileLength.ToString() + " " + "bytes" + ")";
+        }
+
         public DBDetailedInformationPage()
         {
             InitializeComponent();
